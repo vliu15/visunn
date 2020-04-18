c5cb9e12b62c0764e5719d56e5232bcf64084b17
Vincent Liu <36426273+vliu15@users.noreply.github.com>
2020-04-17 17:39:08 -0700

    Fix typo

--- visuai/__init__.py
+++ visuai/__init__.py
@@ -1,7 +1,7 @@
 from visuai.visu import Visu
 from visuai.modu import Modu
 from visuai.plot import plot
-from visuai.util import proto_to_dict, process_nodes, process_modules \
+from visuai.util import proto_to_dict, process_nodes, process_modules, \
                         build_modu
 
 __all__ = ['Visu']
